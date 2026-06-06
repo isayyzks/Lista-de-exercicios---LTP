e569deb600a8136b038643e7796d0908b04201a8
Isa✯ <isabellealves4567@gmail.com>
2026-06-06 16:53:58 -0400

    exercício 11 feito

---
+++ ex11.js
@@ -0,0 +1,43 @@
+class Livro {
+  constructor(titulo, autor, paginas) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.paginas = paginas;
+  }
+
+  toString() {
+    return `Livro: "${this.titulo}" por ${this.autor} (${this.paginas} páginas)`;
+  }
+}
+
+class Filme {
+  constructor(titulo, diretor, duracao) {
+    this.titulo = titulo;
+    this.diretor = diretor;
+    this.duracao = duracao;
+  }
+
+  toString() {
+    return `Filme: "${this.titulo}" dirigido por ${this.diretor} (${this.duracao} min)`;
+  }
+}
+
+class Musica {
+  constructor(titulo, artista, duracao) {
+    this.titulo = titulo;
+    this.artista = artista;
+    this.duracao = duracao;
+  }
+
+  toString() {
+    return `Música: "${this.titulo}" - ${this.artista} (${this.duracao}s)`;
+  }
+}
+
+const itens = [
+    new Livro("Dom Casmurro", "Machado de Assis", 256),
+    new Filme("Como Perder um Homem em 10 Dias", "Donald Petrie", 116),
+    new Musica("Cê Tá Bem", "Dfideliz", 180)
+];
+
+itens.forEach(item => console.log(item.toString()));
